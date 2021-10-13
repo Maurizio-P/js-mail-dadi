10db00120156bb99f468b4819a60275702631b0b
Maurizio-P <mauriziopignotti@gmail.com>
2021-10-13 16:06:59 +0200

    add for

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,16 +1,16 @@
 console.log("inizio esercizio mail")
 
-// const mailInserita = prompt("Inserisci la tua mail")
+const mailInserita = prompt("Inserisci la tua mail")
 const mailConsentite = ["alice", "stefano"]
 
 console.log(`mail consentite: ${mailConsentite}`)
 
-for (let x = 0; 0 <= 2; x++){
+for (let x = 0; x <= mailConsentite.length; x++){
     console.log(x)
-
-//     if (mailInserita === mailConsentite){
-//         alert("benvenuto")
-//     }else {
-//         alert("accesso negato")
-//     }
+    
+    if (mailInserita === mailConsentite[x]){
+        alert("benvenuto")
+    } else  {
+        alert("error")
+    }
 }
