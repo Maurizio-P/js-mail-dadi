523acfdbb467d2b79cf9b41da6a4e6cb6048ea2c
Maurizio-P <mauriziopignotti@gmail.com>
2021-10-13 15:41:53 +0200

    start

---
+++ mail/js/script.js
@@ -0,0 +1,16 @@
+console.log("inizio esercizio mail")
+
+// const mailInserita = prompt("Inserisci la tua mail")
+const mailConsentite = ["alice", "stefano"]
+
+console.log(`mail consentite: ${mailConsentite}`)
+
+for (let x = 0; 0 <= 2; x++){
+    console.log(x)
+
+//     if (mailInserita === mailConsentite){
+//         alert("benvenuto")
+//     }else {
+//         alert("accesso negato")
+//     }
+}
