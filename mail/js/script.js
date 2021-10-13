75d2b22b27cb919f362cd30938468e9640f4d291
Maurizio-P <mauriziopignotti@gmail.com>
2021-10-13 16:53:30 +0200

    complete

--- mail/js/script.js
+++ mail/js/script.js
@@ -3,14 +3,25 @@ console.log("inizio esercizio mail")
 const mailInserita = prompt("Inserisci la tua mail")
 const mailConsentite = ["alice", "stefano"]
 
-console.log(`mail consentite: ${mailConsentite}`)
+console.log(`mail inserita: ${mailInserita} mail consentite: ${mailConsentite}`)
+
+let mailExist = false
 
 for (let x = 0; x <= mailConsentite.length; x++){
     console.log(x)
     
     if (mailInserita === mailConsentite[x]){
-        alert("benvenuto")
-    } else  {
-        alert("error")
-    }
+        mailExist = true
+    } 
+}
+
+if (mailExist) {
+    alert("Benvenuto.")
+    document.querySelector("main").innerHTML += `   <div>
+                                                        <h1>Benvenuto</h1>
+                                                        <p>è bello rivederti.</p>
+                                                    </div>`
+} else {
+    alert("Error. Mail non accettata")
+    
 }
